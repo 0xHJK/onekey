327c4a247196b49aa79fb6e51cbe58031ede100f
HJK <HJKdev@gmail.com>
2018-12-04 14:12:58 +0800

    update ss source

--- install-ssserver.sh
+++ install-ssserver.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# https://github.com/hangim/shadowsocks-docker
 
 SS_PORT='443'
 SS_PASSWORD='123456'
 
-docker run -d --restart=always -e "SS_PORT=${SS_PORT}" -e "SS_PASSWORD=${SS_PASSWORD}" -e "SS_METHOD=chacha20" -e "SS_TIMEOUT=600" -p ${SS_PORT}:${SS_PORT} -p ${SS_PORT}:${SS_PORT}/udp --name ssserver imhang/shadowsocks-docker
+# https://hub.docker.com/r/mritd/shadowsocks/
+docker run -dt --name ss -p ${SS_PORT}:${SS_PORT} mritd/shadowsocks -s "-s 0.0.0.0 -p ${SS_PORT} -m chacha20 -k ${SS_PASSWORD} --fast-open"
+
+
+# https://github.com/hangim/shadowsocks-docker
+# docker run -d --restart=always -e "SS_PORT=${SS_PORT}" -e "SS_PASSWORD=${SS_PASSWORD}" -e "SS_METHOD=chacha20" -e "SS_TIMEOUT=600" -p ${SS_PORT}:${SS_PORT} -p ${SS_PORT}:${SS_PORT}/udp --name ssserver imhang/shadowsocks-docker
